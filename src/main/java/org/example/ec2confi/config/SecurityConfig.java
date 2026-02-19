d5c5ebe2a51ba5b325a780b008ddfc8584a4a4de
NamMau <namtienmau@gmail.com>
2026-02-19 22:41:26 +0700

    added configuration for security

---
+++ src/main/java/org/example/ec2confi/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.example.ec2confi.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.ec2confi.security.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/register", "/saveUser", "/css/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll())
+                .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
