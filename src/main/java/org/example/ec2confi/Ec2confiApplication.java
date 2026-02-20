8efed7fe57cc15362bbd73fb7f2b461932e83bac
NamMau <namtienmau@gmail.com>
2026-02-20 17:09:20 +0700

    added all source files from root project

---
+++ src/main/java/org/example/ec2confi/Ec2confiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ec2confi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2confiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2confiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ec2confi/Ec2confiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ec2confi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2confiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
