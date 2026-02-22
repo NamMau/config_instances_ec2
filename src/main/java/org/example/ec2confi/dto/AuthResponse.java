87ce94ccd9474626f01126475a3133c2536b94eb
NamMau <namtienmau@gmail.com>
2026-02-22 13:20:49 +0700

    added annotation @AllArgsConstructor

--- src/main/java/org/example/ec2confi/dto/AuthResponse.java
+++ src/main/java/org/example/ec2confi/dto/AuthResponse.java
@@ -1,10 +1,13 @@
 package org.example.ec2confi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String message;
     private String username;
