b9a5f1a059486e98731068730b6e4b6232565a7d
NamMau <namtienmau@gmail.com>
2026-02-22 13:00:40 +0700

    added rest api for auth service

---
+++ src/main/java/org/example/ec2confi/controller/AuthRestController.java
@@ -0,0 +1,52 @@
+package org.example.ec2confi.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.ec2confi.entity.User;
+import org.example.ec2confi.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request){
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+
+        userService.registerUser(user);
+        return new AuthResponse(
+                "Register success",
+                request.getUsername()
+        );
+    }
+
+    @GetMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request){
+        boolean authenticated = userService.authenticate(
+                        request.getUsername(),
+                        request.getPassword()
+        );
+
+        if(!authenticated){
+            throw new RuntimeException("Incorrect username or password");
+        }
+
+        return new AuthResponse(
+                "Login success",
+                request.getUsername()
+        );
+    }
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "Auth API is working";
+    }
+}
