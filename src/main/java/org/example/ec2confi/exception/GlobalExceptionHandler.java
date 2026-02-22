40fd824399c4e2cba942734f13cf08ab6421c1f7
NamMau <namtienmau@gmail.com>
2026-02-22 17:52:08 +0700

    created exception handler

---
+++ src/main/java/org/example/ec2confi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.example.ec2confi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "message", ex.getMessage()
+                ));
+    }
+}
