b209204dac922bc197f0094bcfe55e7983de44ed
NamMau <namtienmau@gmail.com>
2026-02-24 01:26:34 +0700

    updated packages

--- src/main/java/org/example/ec2confi/service/FileService.java
+++ src/main/java/org/example/ec2confi/service/FileService.java
@@ -1,15 +1,19 @@
 package org.example.ec2confi.service;
 
-import jakarta.annotation.Resource;
-import org.example.ec2confi.entity.FileMetadata;
+import org.example.ec2confi.dto.FileResponse;
 import org.example.ec2confi.entity.User;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface FileService {
-    FileMetadata saveFile(MultipartFile file, User user) throws IOException;
-    List<FileMetadata> getUserFiles(User user);
+    FileResponse saveFile(MultipartFile file, User user) throws IOException;
+
+    List<FileResponse> getUserFiles(User user);
+
     Resource loadFile(Long fileId, User user);
+
+    void deleteFile(Long fileId, User user) throws IOException;
 }
