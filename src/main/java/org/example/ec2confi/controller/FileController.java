b3af06a7e2d3c70780edd537274044428914fd73
NamMau <namtienmau@gmail.com>
2026-02-24 15:07:25 +0700

    added 2 api post share id and get sharewithme

--- src/main/java/org/example/ec2confi/controller/FileController.java
+++ src/main/java/org/example/ec2confi/controller/FileController.java
@@ -106,4 +106,16 @@ public class FileController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
+    @PostMapping("/share/{fileId}")
+    public ResponseEntity<?> shareFile(@PathVariable Long fileId,
+                                       @RequestParam String targetUsername,
+                                       Principal principal) {
+        fileService.shareFile(fileId, targetUsername, principal.getName());
+        return ResponseEntity.ok("Shared success by " + targetUsername);
+    }
+
+    @GetMapping("/shared-with-me")
+    public ResponseEntity<List<FileResponse>> getSharedWithMe(Principal principal) {
+        return ResponseEntity.ok(fileService.getSharedWithMeFiles(principal.getName()));
+    }
 }
